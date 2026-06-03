d6c0021e74b920f534a44088823aeb54dafabd4b
Alexis <116929396+Alexjohn2023@users.noreply.github.com>
2026-06-03 01:52:01 -0400

    Switch backend from S3 to Terraform Cloud

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
-  backend "s3" {
-    bucket       = "alex-terraform-state-2026"
-    key          = "workspaces-demo/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
-    encrypt      = true
+  cloud {
+    organization = "alexcrh"
+
+    workspaces {
+      name = "terraform-provider-aws"
+    }
   }
 }
